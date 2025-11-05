4d89b02c18626fc940d77cbd29eb95160f46196e
aritra-tech <aritrarick2002@gmail.com>
2025-11-05 23:42:06 +0530

    feat(details): Enable vertical scrolling

--- app/src/main/java/com/aritradas/boxboxclubassignment/ui/screen/DetailScreen.kt
+++ app/src/main/java/com/aritradas/boxboxclubassignment/ui/screen/DetailScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
@@ -73,6 +75,7 @@ fun DetailScreen(
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
+                        .verticalScroll(rememberScrollState())
                         .align(Alignment.Center),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
@@ -126,8 +129,6 @@ fun DetailScreen(
                     CircuitFactsSection(
                         modifier = Modifier.fillMaxWidth()
                     )
-
-                    Spacer(modifier = Modifier.weight(1f))
                 }
             }
         }
